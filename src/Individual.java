6adf9c1c6c2923a0fb6945dbc33a54cda89a77e0
Filippo Wang <wang.filippo02@gmail.com>
2022-05-02 20:31:34 +0200

    SECONDO COMMIT

--- src/Individual.java
+++ src/Individual.java
@@ -1,2 +1,4 @@
 public class Individual {
+
+//TODO    Altro commento random
 }
--- src/Population.java
+++ src/Population.java
@@ -1,2 +1,7 @@
 public class Population {
+
+    //QUESTO è un COMMENTO PER TESTARE I COMMIt
+
+
+    //ma che figo
 }
