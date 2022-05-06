e088e0603357d9bb37089523da2da90c11951165
quellocinese <wang.filippo02@gmail.com>
2022-05-06 11:05:25 +0200

    simone

--- src/Population.java
+++ src/Population.java
@@ -1,12 +1,11 @@
 public class Population {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            System.out.println("TEST");
+            System.out.println("simone");
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println("simone");
         }
     }
 
-    //QUESTO è un COMMENTO PER TESTARE I COMMIt
-
-
-    //ma che figo
 }
