37fe49234c94f454f52235177c78e6a4da9d6e3e
SimonLebron <Simo02ru@gmail.com>
2022-05-06 12:02:20 +0200

    filippo

--- src/Population.java
+++ src/Population.java
@@ -1,11 +1,7 @@
 public class Population {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            System.out.println("simone");
-        }
-        for (int i = 0; i < 5; i++) {
-            System.out.println("simone");
+            System.out.println("Filippo");
         }
     }
-
 }
