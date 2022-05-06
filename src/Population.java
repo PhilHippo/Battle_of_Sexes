e54e3b2a72d77142aaae518b00d1aaa0e28b1d2b
quellocinese <wang.filippo02@gmail.com>
2022-05-06 10:56:49 +0200

    aggiungo un for loop a caso

--- src/Population.java
+++ src/Population.java
@@ -1,6 +1,8 @@
 public class Population {
     public static void main(String[] args) {
-        System.out.println("TEST");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("TEST");
+        }
     }
 
     //QUESTO è un COMMENTO PER TESTARE I COMMIt
