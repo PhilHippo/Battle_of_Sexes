c4ff3a40bc22f58d24ec83945da91a403e2dafa0
Lolpe02 <pellegrino.2007948@studenti.uniroma1.it>
2022-05-23 17:20:25 +0200

    aiuto a ridurre le persone (pulizia etnica)

--- src/Fast.java
+++ src/Fast.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Fast extends Female{
 
-    public Fast(int tag,int age,double points, Population p) {
+    public Fast(int tag,int  age,double points, Population p) {
         super(tag,age,points,p);
     }
 
